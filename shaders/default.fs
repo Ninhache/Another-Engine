5b2b81b00034bc891f1e2a624a2979f4e3079ce3
= <mr.carambar27@gmail.com>
2024-02-09 16:46:16 +0100

    feat: Update shader to be able to test if textures are working

--- shaders/default.fs
+++ shaders/default.fs
@@ -2,6 +2,13 @@
 
 out vec4 FragColor;
 
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 void main() {
-   FragColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);
+	FragColor = mix(texture(texture1, TexCoord) * vec4(ourColor, 1.0), texture(texture2, TexCoord), 0.2);
 }
+
